60cd043405d4f1b9372d5082f3bc27ec8adb9c09
devignasrocys <devignasrocys@gmail.com>
2023-01-10 10:32:15 +0200

    new function

---
+++ komanda/ignas.py
@@ -0,0 +1,9 @@
+def print_name(name):
+    print(f"Sveiki, {name}!")
+
+while True:
+    user_input = input("Koks jusu vardas? ")
+    if user_input != "":
+        print_name(user_input)
+    elif user_input == "":
+        break
