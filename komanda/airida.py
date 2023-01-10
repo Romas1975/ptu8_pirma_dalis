ce35ddcdebb172df7e2f1fd5a3229578af7808f4
airidalabas <airidajuraitiene@gmail.com>
2023-01-10 10:51:32 +0200

    airida_for_ciklas_koreguotas

--- komanda/airida.py
+++ komanda/airida.py
@@ -1,11 +1,12 @@
-ratai = 5
-for skaicius in range(1, 5):
+print("Suskaičiuok iki 3 ;)")
+ratai = 4
+for skaicius in range(1, 4):
     print(skaicius)
     if skaicius > ratai:
         print('ATE')
         break
 else:
-    print("Linkejima visiems!")
+    print("Linkėjimai visiems!")
 
 
 
